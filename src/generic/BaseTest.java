089c26dbe0515ea5fa1bd6a244de230e3ab02bb1
shivkumarroza <shivkumar.roja@gmail.com>
2018-02-23 00:10:46 +0530

    working maven project with grid

--- src/generic/BaseTest.java
+++ src/generic/BaseTest.java
@@ -1,13 +1,18 @@
 package generic;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import bsh.Capabilities;
 
 
 public abstract class BaseTest implements IAutoConst{
@@ -17,11 +22,19 @@ public abstract class BaseTest implements IAutoConst{
 		System.setProperty(CHROME_KEY, CHROME_VALUE);
 		System.setProperty(GECKO_KEY, GECKO_VALUE);
 	}
+	@Parameters({"ip","browser"})
 	
 	@BeforeMethod
-	public void openApplication()
+	public void openApplication(String ip,String browser)
 	{
-		driver=new ChromeDriver();
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setBrowserName(browser);
+		try {
+			driver=new RemoteWebDriver(new URL("http://"+ip+":4444/wd/hub"), cap);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		String appURL=AutoUtil.getProperty(CONFIG_PATH, "appURL");
 		driver.get(appURL);
 		String sITO=AutoUtil.getProperty(CONFIG_PATH, "ITO");
